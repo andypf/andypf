194d272f5e693c6ef709c6ce1c6064dae90ac55c
Andreas Pfau <ap@a-pfau.de>
2024-05-13 08:52:24 +0200

    Create constants.js

---
+++ scripts/constants.js
@@ -0,0 +1,12 @@
+const STATS_KEY = {
+  USER_NAME: "userName",
+  STARS: "stars",
+  ISSUES: "issues",
+  PULL_REQUESTS: "pullRequests",
+  CONTRIBUTED_TO: "contributedTo",
+  COMMITS: "commits",
+  LANGUAGES: "languages",
+  MOST_CONTRIBUTED_TO: "mostContributedTo",
+}
+
+module.exports = { STATS_KEY }
